69c1663353f13001beff74d27bb8895dd50a2621
Marco Santana <marcos.maio96@gmail.com>
2021-11-26 18:04:27 -0300

    Nunjucks and Main Page Route Configured

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
 const server = express();
 
+server.set('view engine', 'html');
+server.use(express.static('public'));
+
+nunjucks.configure('views', {
+    express: server
+});
+
 server.get('/', (req, res) => {
-    return res.send('Olá! tudo certo');
+    return res.render('index');
 });
 
 server.listen(5000, () => {
