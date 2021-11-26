25c595db4f29c7d423c670df3e5a882294f901fc
Marco Santana <marcos.maio96@gmail.com>
2021-11-25 22:50:49 -0300

    Created logic to open and close modal and modal made with dynamic information

---
+++ scripts/scripts.js
@@ -0,0 +1,26 @@
+const cards = document.querySelectorAll('.card');
+const modalOverlay = document.querySelector('.modal-overlay');
+
+
+// const closeModal = document.querySelector('')
+
+for(let card of cards) {
+    card.addEventListener('click', () => {
+        const imgId = card.getAttribute('id');
+        const titleRevenues = card.querySelector('.card-info')
+            .getAttribute('id');
+        const username = card.querySelector('p');
+
+        modalOverlay.classList.add('active');
+        
+        modalOverlay.querySelector('img').src = `../assets/${imgId}.png`;
+        modalOverlay.querySelector('.title-dish').innerHTML = titleRevenues;
+        modalOverlay.querySelector('.name-user').innerHTML = username.innerText;
+    });
+}
+
+modalOverlay.querySelector('.modal-close')
+    .addEventListener('click', () => {
+    modalOverlay.classList.remove('active');
+});
+
