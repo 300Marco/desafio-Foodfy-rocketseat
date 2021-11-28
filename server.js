4f78171b0de29cb5848c113b8e2f85a9d210fe11
Marco Santana <marcos.maio96@gmail.com>
2021-11-28 19:10:19 -0300

    Single recipe page created as well as styles

--- server.js
+++ server.js
@@ -18,11 +18,17 @@ server.get('/revenues', (req, res) => {
     return res.render('revenues', {revenues: data});
 });
 
-server.get('/revenues/:index', (req, res) => {
-    const revenues = data;
-    const revenuesIndex = req.params.index;
+// server.get('/revenues/:index', (req, res) => {
+//     const revenues = data;
+//     const revenuesIndex = req.params.index;
 
-    return res.send(revenues[revenuesIndex]);
+//     const test = data.find()
+
+//     return res.send(revenues[revenuesIndex]);
+// });
+
+server.get('/revenue', (req, res) => {
+    return res.render('revenue', {revenue: data});
 });
 
 server.get('/about', (req, res) => {
