98c48b8bca6b7c5dca0f1bf7f915113ff2f88bc1
Marco Santana <marcos.maio96@gmail.com>
2021-11-28 23:33:36 -0300

    Created logic in buttons to show and hide ingredients

--- public/scripts.js
+++ public/scripts.js
@@ -7,3 +7,52 @@ for(let card of cards) {
     });
 }
 
+hideIngredients();
+hidePreparation();
+hideInformation();
+
+function hideIngredients() {
+    const ingredients = document.querySelector('.ingredients');
+    const ingredientsSpan = document.querySelector('.ingredients span');
+    
+    ingredientsSpan.addEventListener('click', () => {
+        if(ingredientsSpan.innerHTML == "Esconder") {
+            ingredientsSpan.innerHTML = "Mostrar";
+            ingredients.className += " hide";
+        } else {
+            ingredientsSpan.innerHTML = "Esconder";
+            ingredients.className = "ingredients";
+        }
+    });
+}
+
+
+function hidePreparation() {
+    const preparation = document.querySelector('.method-of-preparation');
+    const preparationSpan = document.querySelector('.method-of-preparation span');
+    
+    preparationSpan.addEventListener('click', () => {
+        if(preparationSpan.innerHTML == "Esconder") {
+            preparationSpan.innerHTML = "Mostrar";
+            preparation.className += " hide";
+        } else {
+            preparationSpan.innerHTML = "Esconder";
+            preparation.className = "method-of-preparation";
+        }
+    });
+}
+
+function hideInformation() {
+    const information = document.querySelector(".additional-information");
+    const informationSpan = document.querySelector('.additional-information span');
+
+    information.addEventListener('click', () => {
+        if(informationSpan.innerHTML == "Esconder") {
+            informationSpan.innerHTML = "Mostrar";
+            information.className += " hide";
+        } else {
+            informationSpan.innerHTML = "Esconder";
+            information.className = "method-of-preparation";
+        }
+    });
+}
