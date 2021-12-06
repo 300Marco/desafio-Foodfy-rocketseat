503a1a41d6bab11fd219b083a7a834310844745f
Marco Santana <marcos.maio96@gmail.com>
2021-12-06 10:30:39 -0300

    Comments removed from scripts and deleted data.js files

--- public/scripts.js
+++ public/scripts.js
@@ -1,9 +1,9 @@
-const cards = document.querySelectorAll('.card'); // seleciona todos os cards
+const cards = document.querySelectorAll('.card');
 
 for(let card of cards) {
-    card.addEventListener('click', () => { // ouve o clique em um card
-        const revenueId = card.getAttribute('id'); // pega o id de onde possui a classe "card".
-        window.location.href = `/recipe/${revenueId}`; // envia o endereço para a url
+    card.addEventListener('click', () => { 
+        const revenueId = card.getAttribute('id'); 
+        window.location.href = `/recipe/${revenueId}`; 
     });
 }
 
