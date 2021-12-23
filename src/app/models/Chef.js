b00aadffe5b9caf47eda9fdc3ddf53ed315206ad
Marco Santana <marcos.maio96@gmail.com>
2021-12-23 18:10:38 -0300

    Listing total recipes by chef

--- src/app/models/Chef.js
+++ src/app/models/Chef.js
@@ -4,11 +4,21 @@ const { date } = require('../../lib/utils');
 module.exports = {
     all(callback) {
         db.query(`
-            SELECT * FROM chefs`, (err, results) => {
+            SELECT chefs.*, count(recipes) AS total_recipes 
+            FROM chefs 
+            LEFT JOIN recipes ON (recipes.chef_id = chefs.id)
+            GROUP BY chefs.id
+            ORDER BY total_recipes DESC`, (err, results) => {
                 if(err) throw 'Database Error!';
                 
                 callback(results.rows);
         });
+        // db.query(`
+        //     SELECT * FROM chefs`, (err, results) => {
+        //         if(err) throw 'Database Error!';
+                
+        //         callback(results.rows);
+        // });
     },
     create(data, callback) {
         const query = `
