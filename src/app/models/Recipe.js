0b219e3521dff5c2652d568bff5d19f565671bb4
Marco Santana <marcos.maio96@gmail.com>
2021-12-24 17:32:37 -0300

    Listing chef's name for each recipe

--- src/app/models/Recipe.js
+++ src/app/models/Recipe.js
@@ -3,7 +3,10 @@ const db = require('../../config/db');
 module.exports = {
     all(callback) {
         db.query(`
-            SELECT * FROM recipes`, (err, results) => {
+            SELECT recipes.*, chefs.name AS chefs_name
+            FROM recipes
+            LEFT JOIN chefs ON (recipes.chef_id = chefs.id)
+        `, (err, results) => {
                 if(err) throw 'Database Error!';
 
                 callback(results.rows);
