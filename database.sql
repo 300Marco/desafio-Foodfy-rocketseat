7ce660ae8d76d6eb6cc2c5d98ace69edf7651d54
Marco Santana <marcos.maio96@gmail.com>
2022-02-05 21:48:39 -0300

    database file

---
+++ database.sql
@@ -0,0 +1,33 @@
+CREATE TABLE "chefs" (
+  "id" SERIAL PRIMARY KEY,
+  "name" text,
+  "avatar_url" text,
+  "created_at" timestamp DEFAULT (now())
+);
+
+CREATE TABLE "recipes" (
+  "id" SERIAL PRIMARY KEY,
+  "chef_id" int,
+  "image" text,
+  "title" text,
+  "ingredients" text[],
+  "preparation" text[],
+  "information" text,
+  "created_at" timestamp DEFAULT (now())
+);
+
+CREATE TABLE "files" (
+  "id" SERIAL PRIMARY KEY,
+  "name" text,
+  "path" text NOT NULL
+);
+
+CREATE TABLE "recipe_files" (
+  "id" SERIAL PRIMARY KEY,
+  "recipe_id" int,
+  "file_id" int
+);
+
+ALTER TABLE "recipe_files" ADD FOREIGN KEY ("recipe_id") REFERENCES "recipes" ("id");
+
+ALTER TABLE "recipe_files" ADD FOREIGN KEY ("file_id") REFERENCES "files" ("id");
