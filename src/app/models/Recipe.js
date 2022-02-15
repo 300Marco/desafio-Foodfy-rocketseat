76432b5da8d912c7adaf22b4e8b572a81792fc92
Marco Santana <marcos.maio96@gmail.com>
2022-02-15 10:25:15 -0300

    Code refactoring for try...catch, in recipe models

--- src/app/models/Recipe.js
+++ src/app/models/Recipe.js
@@ -1,50 +1,83 @@
 const db = require('../../config/db');
 
 module.exports = {
-    all(callback) {
-        db.query(`
-            SELECT recipes.*, chefs.name AS chefs_name
-            FROM recipes
-            LEFT JOIN chefs ON (recipes.chef_id = chefs.id)
-        `, (err, results) => {
-                if(err) throw `Database Error! ${err}`;
-
-                callback(results.rows);
-        });
+    all() {
+        try {
+            return db.query(`
+                SELECT recipes.*, chefs.name AS chefs_name
+                FROM recipes
+                LEFT JOIN chefs ON (recipes.chef_id = chefs.id)
+            `);
+        } catch(err) {
+            console.error(err);
+        }
     },
-    find(id, callback) {
-        db.query(`
-            SELECT recipes.*, chefs.name
-            FROM recipes
-            LEFT JOIN chefs ON (recipes.chef_id = chefs.id)
-            WHERE recipes.id = $1`, [id], (err, results) => {
-                if(err) throw `Database Error! ${err}`;
+    // all(callback) {
+    //     db.query(`
+    //         SELECT recipes.*, chefs.name AS chefs_name
+    //         FROM recipes
+    //         LEFT JOIN chefs ON (recipes.chef_id = chefs.id)
+    //     `, (err, results) => {
+    //             if(err) throw `Database Error! ${err}`;
 
-                callback(results.rows);
-        });
+    //             callback(results.rows);
+    //     });
+    // },
+    find(id) {
+        try {
+            return db.query(`
+                SELECT recipes.*, chefs.name
+                FROM recipes
+                LEFT JOIN chefs ON (recipes.chef_id = chefs.id)
+                WHERE recipes.id = $1`, [id]);
+        } catch(err) {
+            console.error(err);
+        }
     },
-    findBy(search, callback) {
-        db.query(`
-            SELECT recipes.*, chefs.name AS chefs_name
-            FROM recipes
-            LEFT JOIN chefs ON (recipes.chef_id = chefs.id)
-            WHERE recipes.title ILIKE '%${search}%'`, (err, results) => {
-                if(err) throw `Database Error! ${err}`;
+    // find(id, callback) {
+    //     db.query(`
+    //         SELECT recipes.*, chefs.name
+    //         FROM recipes
+    //         LEFT JOIN chefs ON (recipes.chef_id = chefs.id)
+    //         WHERE recipes.id = $1`, [id], (err, results) => {
+    //             if(err) throw `Database Error! ${err}`;
 
-                callback(results.rows);
-            });
+    //             callback(results.rows);
+    //     });
+    // },
+    findBy(search) {
+        try {
+            return db.query(`
+                SELECT recipes.*, chefs.name AS chefs_name
+                FROM recipes
+                LEFT JOIN chefs ON (recipes.chef_id = chefs.id)
+                WHERE recipes.title ILIKE '%${search}%'`);   
+        } catch (err) {
+            console.error(err);
+        }
     },
-    totalRecipes(callback) {
-        db.query(`
-            SELECT chefs.*, count(recipes) AS total_recipes 
-            FROM chefs 
-            LEFT JOIN recipes ON (recipes.chef_id = chefs.id)
-            GROUP BY chefs.id
-            ORDER BY total_recipes DESC`, (err, results) => {
-                if(err) throw `Database Error! ${err}`;
-                
-                callback(results.rows);
-        });
+    // findBy(search, callback) {
+    //     db.query(`
+    //         SELECT recipes.*, chefs.name AS chefs_name
+    //         FROM recipes
+    //         LEFT JOIN chefs ON (recipes.chef_id = chefs.id)
+    //         WHERE recipes.title ILIKE '%${search}%'`, (err, results) => {
+    //             if(err) throw `Database Error! ${err}`;
+
+    //             callback(results.rows);
+    //         });
+    // },
+    totalRecipes() {
+        try {
+            return db.query(`
+                SELECT chefs.*, count(recipes) AS total_recipes 
+                FROM chefs 
+                LEFT JOIN recipes ON (recipes.chef_id = chefs.id)
+                GROUP BY chefs.id
+                ORDER BY total_recipes DESC`);
+        } catch(err) {
+            console.error(err);
+        }
     },
     paginate(params) {
         const { search, limit, offset, callback } = params;
