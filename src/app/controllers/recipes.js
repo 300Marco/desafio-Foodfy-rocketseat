6b173f17b73b07af5d561b7fd090920955768756
Marco Santana <marcos.maio96@gmail.com>
2022-03-15 12:32:54 -0300

    Refactoring the logic for listing images on the recipe page

--- src/app/controllers/recipes.js
+++ src/app/controllers/recipes.js
@@ -101,40 +101,82 @@ module.exports = {
             offset
         };
 
-        let files = [];
+        // let files = [];
         let results = await Recipe.paginate(params);
         let recipes = results.rows;
 
-        results = recipes.map(recipe => 
-            File.findRecipeId(recipe.id)
-        );
+        // get images
+        async function getImage(recipeId) {
+            let results = await Recipe.files(recipeId);
+            const files = results.rows.map(
+                file => `${req.protocol}://${req.headers.host}${file.path.replace('public', '')}`
+            );
 
-        let promiseRecipeAndFiles = await Promise.all(results);
+            return files[0];
+        }
 
-        for (file of promiseRecipeAndFiles) {
-            results = await File.findFileForId(file.rows[0].file_id);
-            files.push(results.rows[0]);
-        };
+        const recipesPromise = recipes.map(async recipe => {
+            recipe.img = await getImage(recipe.id);
 
-        files.map(file => 
-            file.src = `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`
-        );
-        
-        for(index in recipes) {
-            recipes[index] = {
-                ...recipes[index],
-                path: files[index].path,
-                src: files[index].src
-            };
-        };
+            return recipe;
+        });
+
+        const lastAdded = await Promise.all(recipesPromise);
 
         const pagination = {
             total: Math.ceil(recipes[0].total / limit),
             page
         };
 
-        return res.render('recipes/recipes', {recipes, pagination});
+        return res.render('recipes/recipes', {recipes: lastAdded, pagination});
     },
+    // async recipes(req, res) {
+    //     let { page, limit } = req.query;
+
+    //     page = page || 1;
+    //     limit = limit || 6;
+    //     let offset = limit * (page -1);
+
+    //     const params = {
+    //         page,
+    //         limit,
+    //         offset
+    //     };
+
+    //     let files = [];
+    //     let results = await Recipe.paginate(params);
+    //     let recipes = results.rows;
+
+    //     results = recipes.map(recipe => 
+    //         File.findRecipeId(recipe.id)
+    //     );
+
+    //     let promiseRecipeAndFiles = await Promise.all(results);
+
+    //     for (file of promiseRecipeAndFiles) {
+    //         results = await File.findFileForId(file.rows[0].file_id);
+    //         files.push(results.rows[0]);
+    //     };
+
+    //     files.map(file => 
+    //         file.src = `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`
+    //     );
+        
+    //     for(index in recipes) {
+    //         recipes[index] = {
+    //             ...recipes[index],
+    //             path: files[index].path,
+    //             src: files[index].src
+    //         };
+    //     };
+
+    //     const pagination = {
+    //         total: Math.ceil(recipes[0].total / limit),
+    //         page
+    //     };
+
+    //     return res.render('recipes/recipes', {recipes, pagination});
+    // },
     // recipes(req, res) {
     //     let { search, page, limit } = req.query;
 
