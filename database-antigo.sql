22ef5c6af74379aeb2808f1a57a29913c56017e4
Marco Santana <marcos.maio96@gmail.com>
2022-03-17 12:52:58 -0300

    Removed project image files. Simple change on a form command line

---
+++ database-antigo.sql
@@ -0,0 +1,36 @@
+CREATE TABLE "chefs" (
+  "id" SERIAL PRIMARY KEY,
+  "file_id" int,
+  "name" text,
+  "created_at" timestamp DEFAULT (now())
+);
+
+CREATE TABLE "recipes" (
+  "id" SERIAL PRIMARY KEY,
+  "chef_id" int,
+  "title" text,
+  "ingredients" text[],
+  "preparation" text[],
+  "information" text,
+  "created_at" timestamp DEFAULT (now())
+);
+
+CREATE TABLE "files" (
+  "id" SERIAL PRIMARY KEY,
+  "name" text,
+  "path" text NOT NULL
+);
+
+CREATE TABLE "recipe_files" (
+  "id" SERIAL PRIMARY KEY,
+  "recipe_id" int,
+  "file_id" int
+);
+
+ALTER TABLE "recipe_files" ADD FOREIGN KEY ("recipe_id") REFERENCES "recipes" ("id");
+
+ALTER TABLE "recipe_files" ADD FOREIGN KEY ("file_id") REFERENCES "files" ("id");
+
+ALTER TABLE "recipes" ADD FOREIGN KEY ("chef_id") REFERENCES "chefs" ("id");
+
+ALTER TABLE "chefs" ADD FOREIGN KEY ("file_id") REFERENCES "files" ("id");
