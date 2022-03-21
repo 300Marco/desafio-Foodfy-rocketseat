bfbe6662e7cb63a87bf8bcb19c9ce43ac1e541c4
Marco Santana <marcos.maio96@gmail.com>
2022-03-20 23:50:26 -0300

    Route index file configured to import and use the new ones from foodfy

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,55 +1,24 @@
 const express = require('express');
 const HomeController = require('../app/controllers/HomeController');
-const RecipesController = require('../app/controllers/RecipesController');
-const AdminRecipesController = require('../app/controllers/AdminRecipesController');
-const ChefsController = require('../app/controllers/ChefsController');
-const multer = require('../app/middlewares/multer');
 const routes = express.Router();
+
+const recipes = require('../routes/recipes');
+const adminRecipes = require('../routes/adminRecipes');
+const adminChefs = require('../routes/adminChefs');
 
 // WEB PAGE
 routes.get('/', HomeController.index);
 // routes.get('/', recipes.index);
 
 // RECIPES
-routes.get('/about', RecipesController.about);
-
-routes.get('/recipes', RecipesController.recipes);
-
-routes.get('/search', RecipesController.search);
-
-routes.get('/chefs', RecipesController.chefs);
-
-routes.get('/details/:index', RecipesController.details);
+routes.use('', recipes);
 
 // ADMIN - RECIPES
-routes.get('/admin/recipes', AdminRecipesController.show);
-
-routes.get('/admin/recipes/create', AdminRecipesController.create);
-
-routes.get('/admin/recipes/:id', AdminRecipesController.details);
-
-routes.get('/admin/recipes/:id/edit', AdminRecipesController.edit);
-
-routes.post("/admin/recipes", multer.array("photos", 5), AdminRecipesController.post);
-
-routes.put('/admin/recipes', multer.array("photos", 5), AdminRecipesController.put);
-
-routes.delete('/admin/recipes', AdminRecipesController.delete);
+routes.use('/admin', adminRecipes);
 
 // ADMIN - CHEFS
-routes.get('/admin/chefs', ChefsController.show);
+routes.use('/admin', adminChefs);
 
-routes.get('/admin/chefs/create', ChefsController.create);
-
-routes.get('/admin/chefs/:id', ChefsController.details);
-
-routes.get('/admin/chefs/:id/edit', ChefsController.edit);
-
-routes.post('/admin/chefs', multer.array("avatar", 1), ChefsController.post);
-
-routes.put('/admin/chefs', multer.array("avatar", 1), ChefsController.put);
-
-routes.delete('/admin/chefs', ChefsController.delete);
 
 routes.use((req, res) => {
     return res.status(404).render('not-found');
