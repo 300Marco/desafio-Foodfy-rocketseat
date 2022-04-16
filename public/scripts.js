ea88899e0f8e99936701a6b65e3e9aecbd816896
Marco Santana <marcos.maio96@gmail.com>
2022-04-16 13:32:40 -0300

    Removing old/commented code in scripts

--- public/scripts.js
+++ public/scripts.js
@@ -7,60 +7,6 @@ for(let card of cards) {
     });
 }
 
-// // Show and hide
-// const ingredients = document.querySelector('.ingredients');
-// const preparation = document.querySelector('.method-of-preparation');
-// const information = document.querySelector(".additional-information");
-
-// if(ingredients || preparation || information) {
-//     hideIngredients();
-//     hidePreparation();
-//     hideInformation();
-// }
-
-// function hideIngredients() {
-//     const ingredientsSpan = document.querySelector('.ingredients span');
-    
-//     ingredientsSpan.addEventListener('click', () => {
-//         if(ingredientsSpan.innerHTML == "Esconder") {
-//             ingredientsSpan.innerHTML = "Mostrar";
-//             ingredients.className += " hide";
-//         } else {
-//             ingredientsSpan.innerHTML = "Esconder";
-//             ingredients.className = "ingredients";
-//         }
-//     });
-// }
-
-
-// function hidePreparation() {
-//     const preparationSpan = document.querySelector('.method-of-preparation span');
-    
-//     preparationSpan.addEventListener('click', () => {
-//         if(preparationSpan.innerHTML == "Esconder") {
-//             preparationSpan.innerHTML = "Mostrar";
-//             preparation.className += " hide";
-//         } else {
-//             preparationSpan.innerHTML = "Esconder";
-//             preparation.className = "method-of-preparation";
-//         }
-//     });
-// }
-
-// function hideInformation() {
-//     const informationSpan = document.querySelector('.additional-information span');
-
-//     information.addEventListener('click', () => {
-//         if(informationSpan.innerHTML == "Esconder") {
-//             informationSpan.innerHTML = "Mostrar";
-//             information.className += " hide";
-//         } else {
-//             informationSpan.innerHTML = "Esconder";
-//             information.className = "method-of-preparation";
-//         }
-//     });
-// }
-
 // Show and hide -- TESTES
 const showAndHide = {
     hideIngredients() {
@@ -218,18 +164,6 @@ function addIngredient() {
             el.parentElement.remove();
         }
     });
-
-    // Não remover por enquanto
-    // const box = document.querySelector('.box-ingredients');
-
-    // btnAddIngredient.addEventListener('click', () => {
-    //     const newInput = document.createElement('input');
-        
-    //     newInput.setAttribute('type', 'text');
-    //     newInput.setAttribute('name', 'ingredients[]');
-
-    //     box.appendChild(newInput);
-    // });
 }
 
 function addPreparation() {
@@ -305,18 +239,6 @@ function addPreparation() {
             el.parentElement.remove();
         }
     });
-
-    // Não remover por enquanto
-    // const box = document.querySelector('.box-preparation');
-
-    // btnAddPreparation.addEventListener('click', () => {
-    //     const newInput = document.createElement('input');
-        
-    //     newInput.setAttribute('type', 'text');
-    //     newInput.setAttribute('name', 'preparation[]');
-
-    //     box.appendChild(newInput);
-    // });
 }
 
 // Pagination
@@ -502,128 +424,6 @@ const ImageGallery = {
     }
 }
 
-// Upload chef files
-// const avatarUpload = {
-//     input: "",
-//     preview: document.querySelector('#avatar-preview'),
-//     uploadLimit: 1,
-//     files: [],
-//     handleFileInput(event) {
-//         const { files: fileList } = event.target;
-//         avatarUpload.input = event.target;
-
-//         if(avatarUpload.hasLimit(event)) return;
-
-//         Array.from(fileList).forEach(file => {
-//             avatarUpload.files.push(file);
-
-//             const reader = new FileReader();
-
-//             reader.onload = () => {
-//                 const image = new Image();
-//                 image.src = String(reader.result);
-
-//                 const linkAvatar = image.src;
-
-//                 // const input = avatarUpload.getContainer(linkAvatar);
-//                 const div = avatarUpload.getContainer(linkAvatar);
-
-//                 // avatarUpload.preview.appendChild(input);
-//                 avatarUpload.preview.appendChild(div);
-//             };
-
-//             reader.readAsDataURL(file);
-//         });
-
-//         avatarUpload.input.files = avatarUpload.getAllFiles();
-//     },
-//     hasLimit(event) {
-//         const { uploadLimit, input, preview } = avatarUpload;
-//         const { files: fileList } = input;
-
-//         if(fileList.length > uploadLimit) {
-//             alert(`Envia no máximo ${uploadLimit} foto`);
-//             event.preventDefault();
-//             return true;
-//         };
-
-//         const avatarDiv = [];
-//         preview.childNodes.forEach(item => {
-//             if(item.classList && item.classList.value == "avatar-box") {
-//                 // console.log('adicionado ao array');
-//                 avatarDiv.push(item);
-//             }
-//         });
-
-//         const totalPhotos = fileList.length + avatarDiv.length;
-//         if(totalPhotos > uploadLimit) {
-//             alert('Não é permitido mais de 1 foto');
-//             event.preventDefault()
-//             return true;
-//         }
-
-//         return false;
-//     },
-//     getAllFiles() {
-//         const dataTransfer = new ClipboardEvent("").clipboardData || new DataTransfer();
-
-//         avatarUpload.files.forEach(file => dataTransfer.items.add(file));
-
-//         return dataTransfer.files;
-//     },
-//     getContainer(linkAvatar) {
-//         const div = document.createElement('div');
-//         const input = document.createElement('input');
-//         div.classList.add('avatar-box');
-//         input.classList.add('avatar');
-
-//         input.value = linkAvatar;
-        
-//         div.appendChild(input);
-//         div.appendChild(avatarUpload.getRemoveButton());
-//         div.onclick = avatarUpload.removeAvatar;
-
-//         return div;
-//         // const div = document.querySelector('#avatar-preview');
-//         // const input = document.createElement('input');
-//         // input.classList.add('avatar');
-
-//         // input.value = linkAvatar;
-        
-//         // div.appendChild(avatarUpload.getRemoveButton());
-//         // div.onclick = avatarUpload.removeAvatar;
-
-//         // return input;
-//     },
-//     getRemoveButton() {
-//         const button = document.createElement('span');
-//         button.innerText = 'x';
-//         return button;
-//     },
-//     removeAvatar(event) {
-//         const avatarDiv = event.target.parentNode;
-//         const avatarArray = Array.from(avatarUpload.preview.children);
-//         const index = avatarArray.indexOf(avatarDiv);
-
-//         avatarUpload.files.splice(index, 1);
-//         avatarUpload.input.files = avatarUpload.getAllFiles();
-
-//         avatarDiv.remove();
-//     },
-//     removedOldAvatar(event) {
-//         const avatarDiv = event.target.parentNode;
-
-//         if(avatarDiv.id) {
-//             const removedAvatar = document.querySelector('input[name="removed_avatar"]');
-
-//             if(removedAvatar) {
-//                 removedAvatar.value += `${avatarDiv.id}`;
-//             }
-//         }
-
-//         avatarDiv.remove();
-//     }
-// }
 const avatarUpload = {
     input: "",
     preview: document.querySelector('#avatar-preview'),
@@ -649,10 +449,8 @@ const avatarUpload = {
 
                 const linkAvatar = image.src;
 
-                // const input = avatarUpload.getContainer(linkAvatar);
                 const div = avatarUpload.getContainer(linkAvatar);
 
-                // avatarUpload.preview.appendChild(input);
                 avatarUpload.preview.appendChild(div);
             };
 
@@ -674,7 +472,6 @@ const avatarUpload = {
         const avatarDiv = [];
         preview.childNodes.forEach(item => {
             if(item.classList && item.classList.value == "avatar-box") {
-                // console.log('adicionado ao array');
                 avatarDiv.push(item);
             }
         });
@@ -708,32 +505,13 @@ const avatarUpload = {
         div.onclick = avatarUpload.removeAvatar;
 
         return div;
-        // const div = document.querySelector('#avatar-preview');
-        // const input = document.createElement('input');
-        // input.classList.add('avatar');
-
-        // input.value = linkAvatar;
-        
-        // div.appendChild(avatarUpload.getRemoveButton());
-        // div.onclick = avatarUpload.removeAvatar;
-
-        // return input;
     },
     getRemoveButton() {
         const button = document.createElement('span');
         button.innerText = 'x';
         return button;
     },
-    removeAvatar(event) {
-        // const avatarDiv = event.target.parentNode;
-        // const avatarArray = Array.from(avatarUpload.preview.children);
-        // const index = avatarArray.indexOf(avatarDiv);
-
-        // avatarUpload.files.splice(index, 1);
-        // avatarUpload.updateInputFiles();
-
-        // avatarDiv.remove();
-        
+    removeAvatar(event) {        
         const avatarDiv = event.target.parentNode;
         const newFiles = Array.from(avatarUpload.preview.children).filter(file => {
             if(file.classList.contains('avatar-box') && !file.getAttribute('id')) return true;
@@ -764,149 +542,3 @@ const avatarUpload = {
         avatarUpload.input.files = avatarUpload.getAllFiles();
     }
 }
-
-// Upload photos chefs
-// const ChefsPhotosUpload = {
-//     input: "",
-//     inputImageButton: document.querySelector('#chefs-photos-upload input'),
-//     sendImageButton: document.querySelector('.add-chef-image-button'),
-//     preview: document.querySelector('#chef-photos-preview'),
-//     uploadLimit: 1,
-//     files: [],
-//     handleFileInput(event) {
-//         const { files: fileList } = event.target;
-//         ChefsPhotosUpload.input = event.target;
-
-//         if(ChefsPhotosUpload.hasLimit(event)) return;
-
-//         Array.from(fileList).forEach(file => {
-//             ChefsPhotosUpload.files.push(file);
-
-//             const reader = new FileReader();
-
-//             reader.onload = () => {
-//                 const image = new Image();
-//                 const imageSrc = image.src = String(reader.result);
-
-//                 const input = ChefsPhotosUpload.getInput(imageSrc);
-
-//                 ChefsPhotosUpload.preview.appendChild(input);
-
-//                 // disabled button send image
-//                 ChefsPhotosUpload.disabledButton();
-//             };
-
-//             reader.readAsDataURL(file);
-//         });
-
-//         ChefsPhotosUpload.input.files = ChefsPhotosUpload.getAvatarFiles();
-//     },
-//     hasLimit(event) {
-//         // const { uploadLimit } = ChefsPhotosUpload;
-//         // const { files: fileList } = event.target;
-//         // const { uploadLimit, input: fileList, preview } = ChefsPhotosUpload;
-//         const { uploadLimit, input, preview } = ChefsPhotosUpload;
-//         const { files: fileList } = input;
-
-//         if(fileList.length > uploadLimit) {
-//             alert(`Envia apenas ${uploadLimit} foto`);
-
-//             event.preventDefault();
-//             return true;
-//         }
-
-//         const photoDiv = 0;
-//         preview.childNodes.forEach(item => {
-//             if(item.classList && item.classList.value == "chef-avatar") {
-//                 photoDiv.push(item);
-//             };
-//         });
-
-//         const totalPhotos = fileList.length + photoDiv.length;
-//         if(totalPhotos > uploadLimit) {
-//             alert('Não é permitido enviar mais de 1 foto');
-//             event.preventDefault();
-//             return true;
-//         }
-
-//         return false;
-//     },
-//     getAvatarFiles() {
-//         const dataTransfer = new ClipboardEvent("").clipboardData || new DataTransfer();
-
-//         ChefsPhotosUpload.files.forEach(file => dataTransfer.items.add(file));
-
-//         return dataTransfer.files;
-//     },
-//     getInput(imageSrc) {
-//         const input = document.createElement('input');
-//         input.classList.add('chef-avatar');
-//         // input.setAttribute('disabled', 'disabled');
-//         input.setAttribute('value', `${imageSrc}`);
-
-//         // input.value = imageSrc;
-//         // input.value = image;
-
-//         const div = document.querySelector('#chef-photos-preview');
-
-//         div.appendChild(ChefsPhotosUpload.getRemoveButton());
-
-//         return input;
-//     },
-//     disabledButton() {
-//         // ChefsPhotosUpload.inputImageButton.setAttribute('disabled', 'disabled');
-//         ChefsPhotosUpload.sendImageButton.classList.add('disabled-chef-button');
-
-//         ChefsPhotosUpload.inputImageButton.style.position = "fixed"
-//         ChefsPhotosUpload.inputImageButton.style.left = "-100%"
-//         // Lightbox.target.style.opacity = 1;
-//     },
-//     activateButton() {
-//         // ChefsPhotosUpload.inputImageButton.removeAttribute('disabled', 'disabled');
-//         ChefsPhotosUpload.sendImageButton.classList.remove('disabled-chef-button');
-
-//         ChefsPhotosUpload.inputImageButton.style.position = "absolute"
-//         ChefsPhotosUpload.inputImageButton.style.left = 0;
-//     },
-//     getRemoveButton() {
-//         const button = document.createElement('button');
-//         button.classList.add('delete-avatar-button');
-//         button.setAttribute('type', 'button');
-//         button.innerHTML = "x";
-
-//         button.onclick = ChefsPhotosUpload.removeAvatar;
-
-//         return button;
-//     },
-//     removeAvatar(event) {
-//         // const avatarInput = event.target.parentNode;
-//         const avatarInput = document.querySelector('.chef-avatar');
-//         const avatarButton = document.querySelector('.delete-avatar-button');
-//         // const avatarArray = Array.from(ChefsPhotosUpload.preview.children);
-//         // const index = avatarArray.indexOf(avatarInput);
-//         const index = 0;
-
-//         ChefsPhotosUpload.files.splice(index, 1);
-//         ChefsPhotosUpload.input.files = ChefsPhotosUpload.getAvatarFiles();
-
-//         avatarInput.remove();
-//         avatarButton.remove();
-
-//         ChefsPhotosUpload.activateButton();
-//     },
-//     removeOldAvatar(event) {
-//         const avatarInput = document.querySelector('.chef-avatar');
-//         const avatarButton = document.querySelector('.delete-avatar-button');
-
-//         if(avatarInput.id) {
-//             const removedAvatar = document.querySelector('input[name="removed_avatar"]');
-
-//             if(removedAvatar) {
-//                 removedAvatar.value = `${avatarInput.id}`;
-
-//                 avatarInput.remove();
-//                 avatarButton.remove();
-//             }
-//         }
-//     }
-// };
