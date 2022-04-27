19b71bd40bc84f168f1a547cfa7b991f0977f6b3
Marco Santana <marcos.maio96@gmail.com>
2022-04-27 15:16:19 -0300

    Setting to display error message to user

--- src/app/validators/adminUser.js
+++ src/app/validators/adminUser.js
@@ -1,11 +1,13 @@
 const User = require('../models/AdminUser');
 
 async function post(req, res, next) {
+    const data = req.body;
+
     // checar se todos os campos estão preenchidos
-    const keys = Object.keys(req.body);
+    const keys = Object.keys(data);
     
         for(key of keys) {
-            if(req.body[key] == "") {
+            if(data[key] == "") {
                 return res.send("Please fill in all fields");
             };
         };
@@ -13,8 +15,14 @@ async function post(req, res, next) {
     // Checar se usuário já existe [email]
     const { email } = req.body;
     const user = await User.findOne({ where: {email} });
+    
+    if(data.is_admin) data.is_admin = 'true';
+    console.log(data)
 
-    if(user) return res.send('Users existis');
+    if(user) return res.render('adminUsers/create', {
+        user: data,
+        error: "Usuário já cadastrado"
+    });
 
     next();
 }
