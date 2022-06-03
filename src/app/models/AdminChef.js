9a78b259021887ed6837067780f02491d45c812b
Marco Santana <marcos.maio96@gmail.com>
2022-06-03 13:43:26 -0300

    Refactoring of chefs controllers, after creating the Base model

--- src/app/models/AdminChef.js
+++ src/app/models/AdminChef.js
@@ -17,37 +17,43 @@ module.exports = {
             console.error(err);
         };
     },
-    files(id) {
+    async files(id) {
         try {
-            return db.query(`
+            const results = await db.query(`
                 SELECT chefs.*, files.path
                 FROM chefs
                 LEFT JOIN files ON (chefs.file_id = files.id)
                 WHERE chefs.id = $1`, [id]);
+            
+                return results.rows;
         } catch(err) {
             console.error(err);
         };
     },
-    findRecipe(id) {
+    async findRecipe(id) {
         try {
-            return db.query(`
+            const results = await db.query(`
                 SELECT recipes.*, chefs.name AS chefs_name
                 FROM recipes
                 LEFT JOIN chefs ON (recipes.chef_id = chefs.id)
                 WHERE recipes.chef_id = $1
                 ORDER BY created_at DESC
             `, [id]);
+
+            return results.rows;
         } catch(err) {
             console.error(err);
         };
     },
-    filesRecipe(id) {
+    async filesRecipe(id) {
         try {
-            return db.query(`
+            const results = await db.query(`
                 SELECT files.*
                 FROM files
                 LEFT JOIN recipe_files ON (files.id = recipe_files.file_id)
                 WHERE recipe_files.recipe_id = $1`, [id]);
+            
+                return results.rows;
         } catch(err) {
             console.error(err);
         };
